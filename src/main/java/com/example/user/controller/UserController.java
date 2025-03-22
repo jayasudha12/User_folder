8db4181f69834114af7af81828b0ae6bd2326e54
jayasudha12 <jayasudha.t2022cse@sece.ac.in>
2025-03-22 15:03:59 +0530

    commit message

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.example.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.example.user.entity.User;
 import com.example.user.security.JwtUtil;
 import com.example.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -35,6 +45,11 @@ public class UserController {
     public List<User> getUsers(@RequestHeader("Authorization") String token) {
         validateToken(token);
         return userService.getUsers();
+    }
+    @GetMapping("/getdetails")
+    public List<User> getdetails() {
+       
+        return userService.getdetails();
     }
 
     @PutMapping("/update/{id}")
--- src/main/java/com/example/user/service/UserService.java
+++ src/main/java/com/example/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.example.user.service;
 
-import com.example.user.entity.User;
-import com.example.user.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.user.entity.User;
+import com.example.user.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -26,6 +27,9 @@ public class UserService {
     }
 
     public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+    public List<User> getdetails() {
         return userRepository.findAll();
     }
 
